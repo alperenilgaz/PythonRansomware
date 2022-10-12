e52e8c0569e0ccd99d855fb12311aeed3ab901fc
alperenilgaz <alperenilgaz3@gmail.com>
2022-10-12 10:49:28 -0400

    first commit

---
+++ ransomdecrypter.py
@@ -0,0 +1,19 @@
+import os
+from cryptography.fernet import Fernet
+file_list=[]
+for file in os.listdir():
+    if file=="ransom.py" or file=="generatedkey.key" or file=="ransomdecrypter.py":
+        continue
+    if os.path.isfile(file):
+        file_list.append(file)
+
+
+with open("generatedkey.key","rb") as generatedkey:
+    secret_key=generatedkey.read()
+
+for file in file_list:
+    with open(file,"rb") as the_file:
+        contents=the_file.read()
+    contents_decrypyted=Fernet(secret_key).decrypt(contents)
+    with open(file,"wb") as the_file:
+        the_file.write(contents_decrypyted)
